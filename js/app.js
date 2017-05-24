2dff489ae9661c7c2ead10a6b015fba1552237b8
ABIGAIL CHANSLER <ABIGAIL.CHANSLER@GENERALASSEMB.LY>
2017-05-23 22:12:55 -0400

    final background styles

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('leaf', 'assets/leaf.png');
-    game.load.image('dino', 'assets/dino.png');
+    game.load.spritesheet('dino', 'assets/fullDino.png', 80, 62);
 		game.load.image('rock', 'assets/rock.png');
 		game.load.image('comet', 'assets/comet.png');
 
@@ -18,12 +18,12 @@ var leaves;
 var score = 0;
 var scoreText;
 var rock;
-var gameOverText;
+var gameOverWinText;
+var gameOverLoseText;
 var style;
 var comet;
 
 function create() {
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.sprite(0, 0, 'sky');
@@ -45,9 +45,10 @@ function create() {
     ledge.body.immovable = true;
 
     player = game.add.sprite(32, game.world.height - 150, 'dino');
-
+    player.animations.add('left', [0]);
+    player.animations.add('right', [2]);
+    player.animations.add('stop', [1]);
     game.physics.arcade.enable(player);
-
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
@@ -67,18 +68,26 @@ function create() {
         leaf.body.bounce.y = 0.7 + Math.random() * 0.1;
     }
 
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#ffffff' });
 
     cursors = game.input.keyboard.createCursorKeys();
 
     game.time.events.repeat(Phaser.Timer.SECOND * 2, 100, createComet, this);
 
-    var style = { font: 'bold 32px Arial', fill: '#ffffff', align: 'center', boundsAlignH: 'center', boundsAlignV: 'middle' };
+
+    var style = { font: 'bold 32px Arial', fill: '#ffffff', align: 'center', boundsAlignH: 'center', boundsAlignV: 'middle', backgroundColor: 'rgba(0,255,0,0.25)' };
 
     text = game.add.text(150, 150, '- Dino Hoodwinked -\nSave Little Dino! Avoid the comets \nand collect leaves to survive. \nOnce you have enough leaves, \nhide under the rock for safety. \nClick to start!', style);
 
-    gameOverText = game.add.text(150, 150, '- YOU WIN! - \n Great job saving Little Dino \nfor now. Dinosaurs go extinct \nanyway though, so... yeah...', style);
-    gameOverText.visible = false;
+    gameOverWinText = game.add.text(150, 150, '- YOU WIN! - \n Great job saving Little Dino \nfor now. Dinosaurs go extinct \nanyway though, so... yeah... \n Click to play again!', style);
+    gameOverWinText.visible = false;
+
+    gameOverLoseText = game.add.text(220, 150, '- YOU LOSE - \n LITTLE DINO IS DEAD \n Click to play again!', style);
+    gameOverLoseText.visible = false;
+
+    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    gameOverLoseText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    gameOverWinText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
     game.input.onDown.addOnce(removeText, this);
 
@@ -88,7 +97,7 @@ function create() {
 function update() {
     game.physics.arcade.collide(player, platforms);
     game.physics.arcade.collide(leaves, platforms);
-    game.physics.arcade.overlap(rock, player, gameOver);
+    game.physics.arcade.overlap(rock, player, gameOverWin);
     game.physics.arcade.overlap(player, leaves, collectLeaf, null, this);
     game.physics.arcade.overlap(player, comet, cometHitsPlayer, null, this);
 
@@ -106,8 +115,7 @@ function update() {
     }
     else
     {
-        player.animations.stop();
-        player.frame = 4;
+        player.animations.play('stop');
     }
 
     if (cursors.up.isDown && player.body.touching.down)
@@ -127,25 +135,38 @@ function collectLeaf(player, leaf) {
   score += 10;
   scoreText.text = 'Score: ' + score;
 
-
   if(score >= 100) {
       rock.visible = true;
     }
 }
 
 function createComet() {
-  var comet = game.add.sprite(game.world.randomX, 0, 'comet');
+  var randomX = game.world.randomX;
+  comet = game.add.sprite(randomX, 0, 'comet');
   game.physics.arcade.enable(comet);
   comet.enableBody = true;
   comet.body.gravity.y = 500;
+  comet.body.velocity.x = 200;
+
+  if(randomX >= 400) {
+    comet.body.velocity.x = -200;
+  }
 }
 
 function cometHitsPlayer(player, comet) {
-  console.log('hi');
+  player.kill();
+  gameOverLoseText.visible = true;
+  setupClickToRestart();
 }
 
 
-function gameOver (rock, player) {
-  gameOverText.visible = true;
+function gameOverWin(rock, player) {
+  gameOverWinText.visible = true;
+  setupClickToRestart();
+}
 
+function setupClickToRestart() {
+  game.input.onDown.addOnce(function() {
+    game.state.start(game.state.current);
+  }, this);
 }
